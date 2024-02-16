4fb52a2156708b492926d0ebe978383d93801e7e
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2024-02-16 15:03:57 -0300

    Add files via upload

---
+++ soma.php
@@ -0,0 +1,14 @@
+<?php
+
+
+   $num1 = 10;
+   $num2 = 20;
+
+
+   $soma = $num1 + $num2;
+
+   echo "A soma destes números é: " . $soma . "\n";
+
+   echo gettype($soma) . "\n";
+
+   echo is_integer($num2) . "\n";
