ef29f56e09f808f56b1e5672250c009e69c9b92c
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2024-02-23 15:10:31 -0300

    Update and rename ex2 to ex2.php

--- 1º Bimestre/Aula 3:/ex2.php
+++ 1º Bimestre/Aula 3:/ex2.php
@@ -9,14 +9,13 @@ function CalcularCircunferencia($r) {
     return $circunferencia;
 }
 
-// Lendo e calculando para três círculos
 for ($i = 0; $i < 3; $i++) {
     $r = readline("Informe o raio do círculo em centímetros: " . "\n");
     
     $a = CalcularAreaCirculo($r);
     $circunferencia = CalcularCircunferencia($r);
     
-    echo "Para o círculo com raio " . $r . "cm" . "\n";
-    echo "Área: " .  $a . "cm²" . "\n";
-    echo "Circunferência: " .  $circunferencia . "cm" . "\n";
+    echo "Para o círculo com raio " . $r . "cm." . "\n";
+    echo "Área: " .  $a . "cm²." . "\n";
+    echo "Circunferência: " .  $circunferencia . "cm." . "\n";
 }
