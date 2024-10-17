2d120600d313a1e6f30ec0e381a41981f6eaa198
Mayara Navakoviski <mayara.navakoviski.tds2023@gmail.com>
2024-10-17 15:33:20 -0300

    Update Prato.php

--- 2º  Bimestre/Atividade avaliativa/Modelo/Prato.php
+++ 2º  Bimestre/Atividade avaliativa/Modelo/Prato.php
@@ -1,51 +1,50 @@
 <?php
 
+//  Começo da CLASS:
 class Prato {
-    
+
+    // ATRIBUTOS:
     private int $numero;
     private string $nome;
     private float $valor;
 
+    // CONSTRUCT
     public function __construct($numero, $nome, $valor) {
         $this->numero = $numero;
         $this->nome = $nome;
         $this->valor = $valor;
     }
 
+    // GETs e SETs
     public function getNumero(): int {
         return $this->numero;
     }
-
     public function setNumero(int $numero): self {
         $this->numero = $numero;
-
         return $this;
     }
 
     public function getNome(): string {
         return $this->nome;
     }
-
     public function setNome(string $nome): self {
         $this->nome = $nome;
-
         return $this;
     }
 
     public function getValor(): float {
         return $this->valor;
     }
-
     public function setValor(float $valor): self {
         $this->valor = $valor;
-
         return $this;
     }
 
+    // MÉTODOS
     public function __toString() {
         $imprimir = "Número: " . $this->getNumero() . "\n" . 
-        "Nome: " . $this->getNome() . "\n". 
-        "Valor: " . $this->getValor(). "\n\n";
+        "Nome: " . $this->getNome() . "\n" . 
+        "Valor: " . $this->getValor(). "\n" . "\n";
 
         return $imprimir;
     }
